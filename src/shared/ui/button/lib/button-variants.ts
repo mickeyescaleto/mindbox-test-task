53acc598f800cbabd7482f01fdb277dcac3413d8
mickeyescaleto <mickeyescaleto@gmail.com>
2025-06-03 02:55:47 +0300

    feat: update button components

--- src/shared/ui/button/lib/button-variants.ts
+++ src/shared/ui/button/lib/button-variants.ts
@@ -1,11 +1,12 @@
 import { cva } from 'class-variance-authority';
 
 export const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xs text-sm transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-accent focus-visible:ring-accent focus-visible:ring-2",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xs text-sm transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-accent focus-visible:ring-2",
   {
     variants: {
       variant: {
         ghost: 'hover:bg-accent',
+        outline: 'border border-accent hover:bg-accent',
         text: 'text-text/75 hover:text-text',
       },
       size: {
