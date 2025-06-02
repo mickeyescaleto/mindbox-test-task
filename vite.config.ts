a4c136ec21072699588a86808d42c25132e9e1ea
mickeyescaleto <mickeyescaleto@gmail.com>
2025-06-02 22:20:53 +0300

    chore: set up path aliases via vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [tsconfigPaths(), react(), tailwindcss()],
 });
